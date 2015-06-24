f650cfe2955c0237a18d22900f738c742dba02aa
mengdong.wang <mengdong.wang@dianping.com>
2015-06-24 14:48:21 +0800

    添加facade和quote,update v

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ var makeArray = require("make-array");
 var  argv =  process.argv;
 var rootPath = process.cwd();
 var path =  require("path");
+// var compiler = require('cortex-handlebars-compiler');
 
 var DIR_NANME;
-var cssLinkRxg = /<link\s+(?:rel\=[\"\']stylesheet[\"\']\s+)?(?:type\=[\"\']text\/css[\"\']\s+)?href\=[\"\']\{{3}([\w\.\/\'\"\s\-]+)\}{3}[\"\']\s*\/?>/g,
+var cssLinkRxg = /<link\s+(?:rel\=[\"\']stylesheet[\"\']\s+)?(?:type\=[\"\']text\/css[\"\']\s+)?href\=[\"\']\{{3}([\w\.\/\'\"\s\-]+)\}{3}[\"\']\s*\/>/g,
 	hrefRxg = /\{{3}\s*(static|modfile)\s*[\'\"]([\w\.\/\s\-]+)[\'\"]\s*\}{3}/,
-	imgHrefRxg = /<img\s+src\=[\'\"]\{{3}(static|modfile)\s+[\'\"]([\w\.\/\'\"\s\-]+)[\'\"]\s*\}{3}[\'\"]\s*\/?>/g,
+	imgHrefRxg = /<img\s+src\=[\'\"]\{{3}(static|modfile)\s+[\'\"]([\w\.\/\'\"\s\-]+)[\'\"]\s*\}{3}[\'\"]\s*\/>/g,
 	bgHrefRxg = /url\([\'\"]?([\w\.\/\s\-]+)[\'\"]?\)/g,
 	bgSrcRxg = /url\([\'\"]?([\w\.\/\s\-]+)[\'\"]?\)/,
 	jsSrcRxg = /\{{3}\s*(facade)\s*[\'\"]([\w\.\/\s\-]+)[\'\"]\s*\}{3}/;
 
-
 function readHTMLFile(file,filePath){
-	
+
 	var fileStream = file;
+
 	var cssLinkArr = fileStream.match(cssLinkRxg);
 	var img2HTMLLinkArr = fileStream.match(imgHrefRxg);
 	//html中所有的css路径
@@ -24,6 +25,9 @@ function readHTMLFile(file,filePath){
 	var cssArr = readLink(cssLinkArr).map(function(item){
 		return item.link;
 	})
+
+	//html中的js路径
+	//var js2HtmlArr = makeJsArr(file,filePath);
 
 	//html路径
 	var htmlArr = makeHtmlArr(filePath);
@@ -39,21 +43,22 @@ function readHTMLFile(file,filePath){
 
 	imgArr = imgArr.concat(img2CssArr);
 
-	//cortex.js中依赖的js
-	var jsArr = readJson(fileStream);
+	//cortex.js中依赖的js，外部资源和html中的js路径
+	var jsArr = concatArr(readJson(), makeJsArr(file,filePath));
 
 	var maps = concatArr(cssArr, imgArr, jsArr, htmlArr);
 
-	// var cachename = file.replace("html","appcache");
+	var cachename = file.replace("html","appcache");
 
 	//writeFile(cachename, maps)
+	
 	var data = "CACHE MANIFEST\n";
 
 	maps.forEach(function(item){
 		data += item + "\n";
 	})
 
-	data += "NETWORK:\n*\n"
+	data += "NETWORK:\n*\n";
 
 	return new Buffer(data);
 
@@ -62,15 +67,15 @@ function readHTMLFile(file,filePath){
 
 function readImg2Css(arr){
 	var imgArr = [], imgStrArr = [];
-	arr.forEach(function(cssItem){
+	arr.forEach(function(item){
 		
-		if(cssItem.type == "modfile"){
+		if(item.type == "modfile"){
 			return ;
 		}
 
 
 		var csslink = "",
-			cssLinkArr = cssItem.path.split("/");
+			cssLinkArr = item.path.split("/");
 
 		for(var i = 0; i< cssLinkArr.length; i++){
 			if(cssLinkArr[i] != ".."){
@@ -83,13 +88,10 @@ function readImg2Css(arr){
 
 		makeArray(imgSrcArr).forEach(function(item){
 
-			var imgPath = bgSrcRxg.exec(item)[1];
-			if (!path.isAbsolute(imgPath)) {
-				imgPath = path.join(path.dirname(cssItem.path), imgPath);
+			if(imgArr.indexOf(bgSrcRxg.exec(item)[1]) == -1){
+				imgArr.push(bgSrcRxg.exec(item)[1]);
 			}
-			if(imgArr.indexOf(imgPath) == -1){
-				imgArr.push(imgPath);
-			}
+
 		})
 	})
 
@@ -112,25 +114,70 @@ function autoUpdate(str){
 	return str+"?"+new Date().getTime();
 }
 
+function makeJsArr(fileStream,filePath) {
 
+	var jsLink = fileStream.match(jsSrcRxg)[2];
 
-function readJson(stream){
+	var js2HtmlArr = jsLink.split("/"),
+		htmlPathArr = filePath.split("/"),
+		rootPathArr = rootPath.split("/");
 
-	
+	var relativePathArr = [];
+	var relativeJsPathArr = [];
 
-	var jsArr = [];
-	var dependencies = JSON.parse(fs.readFileSync(rootPath+"/cortex.json","utf-8")).dependencies;
+	htmlPathArr.forEach(function(item){
+		if(rootPathArr.indexOf(item) == -1){
+			relativePathArr.push(item);
+		}
+	});
 
-	for(var i in dependencies){
-		jsArr.push("{{{modfile '" + i + "'}}}");
+	js2HtmlArr.forEach(function(item){
+
+		if(rootPathArr.indexOf(item) == -1){
+			relativeJsPathArr.push(item);
+		}
+
+	})
+
+	var num = relativePathArr.length 
+
+	if(num == 1){
+		relativeJsPathArr.splice(".");
+	}else{
+		for(var i = 1; i < relativePathArr.length; i++ ){
+			relativeJsPathArr.splice(0, 0, "..");
+		}
 	}
 
-	return jsArr;
+	return "{{{static '" + relativeJsPathArr.join("/") + ".js'}}}";
+
+
+}
+
+
+
+function readJson(){
+
+	var cortexArr = [];
+	var cortexJson = JSON.parse(fs.readFileSync(rootPath+"/cortex.json","utf-8"))
+	var dependencies = cortexJson.dependencies;
+	var quoteResource = cortexJson.quoteResource || [];
+	
+	for(var i in dependencies){
+		cortexArr.push("{{{modfile '" + i + "'}}}");
+	}
+
+	for(var j = 0; j < quoteResource.length; j++){
+
+		cortexArr.push(quoteResource[j]);
+
+	}
+
+	return cortexArr;
 
 }
 
 function concatArr(){
-
 	var arr = []
 	for(var i = 0 ;i < arguments.length; i++){
 		arr = arr.concat(arguments[i]);
@@ -147,7 +194,7 @@ function readLink(arr){
 	makeArray(arr).forEach(function(item){
 		var match = hrefRxg.exec(item);
 		if(!match)
-			return;
+			return ;
 		var nameArr = match[2].split("/"),
 			name = nameArr[nameArr.length - 1];
 		newArr.push({"link": match[0], "path": match[2], "name": name,"type":match[1]});
@@ -156,12 +203,5 @@ function readLink(arr){
 
 }
 
-function test () {
-
-	
-
-}
-
 
 module.exports =  readHTMLFile;
-
